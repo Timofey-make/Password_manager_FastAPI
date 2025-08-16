d4b33c71439a3cb65dcb6f24e8f356208f24bf9c
Тима <nobo0000x@gmail.com>
2025-08-16 16:06:31 +0700

    Переделан init.py

--- src/init.py
+++ src/init.py
@@ -1,25 +1,39 @@
-import sqlite3
+from sqlalchemy import create_engine, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-def init_db():
-    with sqlite3.connect("users.db") as conn:
-        cursor = conn.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT UNIQUE NOT NULL,
-        password TEXT NOT NULL)''')
-        cursor.execute('''CREATE TABLE IF NOT EXISTS passwords (
-        categories TEXT,
-        user_id INTEGER NOT NULL,
-        name TEXT NOT NULL,
-        username TEXT NOT NULL,
-        password TEXT NOT NULL)
-        ''')
-        cursor.execute('''CREATE TABLE IF NOT EXISTS share (
-        ownername INTEGER NOT NULL,
-        sendername TEXT NOT NULL,
-        name TEXT NOT NULL,
-        username TEXT NOT NULL,
-        password TEXT NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
-        ''')
-        conn.commit()
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(30))
+    password: Mapped[str] = mapped_column(String(30))
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.username!r}, password={self.password!r})"
+
+class Password(Base):
+    __tablename__ = "passwords"
+    categories: Mapped[str]
+    user_id: Mapped[str]
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    username: Mapped[str]
+    password: Mapped[str]
+    def __repr__(self) -> str:
+        return f"Password(categories={self.categories!r}, user_id={self.user_id!r}, name={self.name!r}, username={self.username!r}, password={self.password!r})"
+
+class Share(Base):
+    __tablename__ = "share"
+    ownername: Mapped[int]
+    sendername: Mapped[str]
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    username: Mapped[str]
+    password: Mapped[str]
+    def __repr__(self) -> str:
+        return f"Share(ownername={self.ownername!r}, sendername={self.sendername!r}, name={self.name!r}, username={self.username!r}, password={self.password!r})"
+
+
+engine = create_engine("sqlite:///users.db", echo=False)
